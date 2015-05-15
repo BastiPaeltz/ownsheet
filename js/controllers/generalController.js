f37da97bc84648030e5f3c6c4bae1223c6eb5e7b
BastiPaeltz <bastipaeltz@googlemail.com>
2015-05-16 00:06:17 +0200

    working on import export feature

--- js/controllers/generalController.js
+++ js/controllers/generalController.js
@@ -10,21 +10,28 @@
 
 var ownsheetApp = angular.module("ownsheetApp");
 
-ownsheetApp.controller('generalController', ["$scope", "$window", "localStorageService",
-    function ($scope, $window, localStorageService) {
+ownsheetApp.controller('generalController', ["$scope", "$window", "chromeStorageService", "localStorageService",
+    function ($scope, $window, chromeStorageService, localStorageService) {
         $scope.colors = localStorageService.get('colors') || [
                 {code: "#2d9f34"}, {code: "#4b65c3"}, {code: "#48456a"}, {code: "#4f7a4e"},
                 {code: "#d61115"}, {code: "#59582f"}];
 
+        document.title="Explore ownsheet";
         this.addFont = function () {
             $scope.colors.push({code: "Enter a hex color code."})
         };
 
         this.removeFont = function () {
             $scope.colors.pop();
+            if($scope.colors.length < 1) {
+                $scope.colors = [
+                    {code: "#2d9f34"}, {code: "#4b65c3"}, {code: "#48456a"}, {code: "#4f7a4e"},
+                    {code: "#d61115"}, {code: "#59582f"}];
+            }
         };
 
         this.submit = function () {
+
             for(var index in $scope.colors){
                 // test if valid hex color
                 if(/^#[0-9A-F]{6}$/i.test($scope.colors[index].code) === false){
@@ -40,7 +47,26 @@ ownsheetApp.controller('generalController', ["$scope", "$window", "localStorageS
                 {code: "#2d9f34"}, {code: "#4b65c3"}, {code: "#48456a"}, {code: "#4f7a4e"},
                 {code: "#d61115"}, {code: "#59582f"}];
             localStorageService.set('colors', $scope.colors)
+        };
+
+        this.import = function () {
+
+            $scope.colors = [
+                {code: "#2d9f34"}, {code: "#4b65c3"}, {code: "#48456a"}, {code: "#4f7a4e"},
+                {code: "#d61115"}, {code: "#59582f"}];
+            localStorageService.set('colors', $scope.colors)
+        };
+
+        this.export = function () {
+
+            var entireStoragePromise = chromeStorageService.getFromStorage(null);
+            entireStoragePromise.then(function(value){
+                var blob = new Blob([JSON.stringify(value)], {type: "application/json;charset=utf-8"});
+                saveAs(blob, "ownsheet-content.json");
+            });
         }
+
+
 
     }]);
 
