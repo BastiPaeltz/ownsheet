fc6b5b627993a04c4280d529a1975b2e8a37c105
BastiPaeltz <bastipaeltz@googlemail.com>
2015-05-21 17:45:27 +0200

    spinner added

--- js/controllers/viewController.js
+++ js/controllers/viewController.js
@@ -14,6 +14,9 @@ ownsheetApp.controller('viewController', ["$scope", "$window", "$routeParams", "
         var sheetNameParam = $routeParams.sheetName;
         $scope.sheet = {};
 
+        startSpinner();
+
+        // case: preview
         if ($window.location.href.endsWith('main.html#/preview')) {
             $scope.sheet.name = "preview";
             $scope.mode = "preview";
@@ -124,3 +127,26 @@ function renderContent(mdContent, mdParserService) {
 String.prototype.endsWith = function (suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
+
+function startSpinner(){
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 20, // The length of each line
+        width: 10, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#000', // #rgb or #rrggbb or array of colors
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: '50%', // Top position relative to parent
+        left: '50%' // Left position relative to parent
+    };
+    var target = document.getElementById('ms-content');
+    var spinner = new Spinner(opts).spin(target);
+}
